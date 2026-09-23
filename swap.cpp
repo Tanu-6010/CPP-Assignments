b636aa89654d292b03b6ca0cb55e703905b9bccb
Tanu-6010 <tanisha07rasal@gmail.com>
2026-09-22 22:21:24 -0700

    Implement swap functionality for two numbers
    Fix variable assignment and output formatting.

---
+++ swap.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+cout<<"Enter first number (a):";
+cin>>a;
+cout<<"Enter second number (b):";
+cin>>b;
+cout<<"\nBefore swapping: a="<<a<<",b="<<b<<endl;
+c=a
+a=b
+b=c
+cout<<"After swapping:a="<<a<<",b="<<b<<endl;
+return 0;
+}
